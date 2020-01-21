c423461c283b08394d2da7d424671757bbe3f68f
luoChen <hank.luo@xianghuanji.com>
2020-01-21 11:59:03 +0800

    ..

--- leakcanary-master/leakcanary-master/leakcanary-object-watcher/src/main/java/leakcanary/ObjectWatcher.kt
+++ leakcanary-master/leakcanary-master/leakcanary-object-watcher/src/main/java/leakcanary/ObjectWatcher.kt
@@ -34,162 +34,166 @@ import java.util.concurrent.Executor
 // Thread safe by locking on all methods, which is reasonably efficient given how often
 // these methods are accessed.
 class ObjectWatcher constructor(
-  private val clock: Clock,
-  private val checkRetainedExecutor: Executor,
-  /**
-   * Calls to [watch] will be ignored when [isEnabled] returns false
-   */
-  private val isEnabled: () -> Boolean = { true }
+        private val clock: Clock,
+        private val checkRetainedExecutor: Executor,
+        /**
+         * Calls to [watch] will be ignored when [isEnabled] returns false
+         */
+        private val isEnabled: () -> Boolean = { true }
 ) {
 
-  private val onObjectRetainedListeners = mutableSetOf<OnObjectRetainedListener>()
+    private val onObjectRetainedListeners = mutableSetOf<OnObjectRetainedListener>()
 
-  /**
-   * References passed to [watch].
-   */
-  private val watchedObjects = mutableMapOf<String, KeyedWeakReference>()
+    /**
+     * References passed to [watch].
+     */
+    private val watchedObjects = mutableMapOf<String, KeyedWeakReference>()
 
-  private val queue = ReferenceQueue<Any>()
+    private val queue = ReferenceQueue<Any>()
 
-  /**
-   * Returns true if there are watched objects that aren't weakly reachable, and
-   * have been watched for long enough to be considered retained.
-   */
-  val hasRetainedObjects: Boolean
-    @Synchronized get() {
-      removeWeaklyReachableObjects()
-      return watchedObjects.any { it.value.retainedUptimeMillis != -1L }
-    }
-
-  /**
-   * Returns the number of retained objects, ie the number of watched objects that aren't weakly
-   * reachable, and have been watched for long enough to be considered retained.
-   */
-  val retainedObjectCount: Int
-    @Synchronized get() {
-      removeWeaklyReachableObjects()
-      return watchedObjects.count { it.value.retainedUptimeMillis != -1L }
-    }
-
-  /**
-   * Returns true if there are watched objects that aren't weakly reachable, even
-   * if they haven't been watched for long enough to be considered retained.
-   */
-  val hasWatchedObjects: Boolean
-    @Synchronized get() {
-      removeWeaklyReachableObjects()
-      return watchedObjects.isNotEmpty()
-    }
-
-  /**
-   * Returns the objects that are currently considered retained. Useful for logging purposes.
-   * Be careful with those objects and release them ASAP as you may creating longer lived leaks
-   * then the one that are already there.
-   */
-  val retainedObjects: List<Any>
-    @Synchronized get() {
-      removeWeaklyReachableObjects()
-      val instances = mutableListOf<Any>()
-      for (weakReference in watchedObjects.values) {
-        if (weakReference.retainedUptimeMillis != -1L) {
-          val instance = weakReference.get()
-          if (instance != null) {
-            instances.add(instance)
-          }
+    /**
+     * Returns true if there are watched objects that aren't weakly reachable, and
+     * have been watched for long enough to be considered retained.
+     */
+    val hasRetainedObjects: Boolean
+        @Synchronized get() {
+            removeWeaklyReachableObjects()
+            return watchedObjects.any { it.value.retainedUptimeMillis != -1L }
         }
-      }
-      return instances
+
+    /**
+     * Returns the number of retained objects, ie the number of watched objects that aren't weakly
+     * reachable, and have been watched for long enough to be considered retained.
+     */
+    val retainedObjectCount: Int
+        @Synchronized get() {
+            removeWeaklyReachableObjects()
+            return watchedObjects.count { it.value.retainedUptimeMillis != -1L }
+        }
+
+    /**
+     * Returns true if there are watched objects that aren't weakly reachable, even
+     * if they haven't been watched for long enough to be considered retained.
+     */
+    val hasWatchedObjects: Boolean
+        @Synchronized get() {
+            removeWeaklyReachableObjects()
+            return watchedObjects.isNotEmpty()
+        }
+
+    /**
+     * Returns the objects that are currently considered retained. Useful for logging purposes.
+     * Be careful with those objects and release them ASAP as you may creating longer lived leaks
+     * then the one that are already there.
+     */
+    val retainedObjects: List<Any>
+        @Synchronized get() {
+            removeWeaklyReachableObjects()
+            val instances = mutableListOf<Any>()
+            for (weakReference in watchedObjects.values) {
+                if (weakReference.retainedUptimeMillis != -1L) {
+                    val instance = weakReference.get()
+                    if (instance != null) {
+                        instances.add(instance)
+                    }
+                }
+            }
+            return instances
+        }
+
+    @Synchronized
+    fun addOnObjectRetainedListener(listener: OnObjectRetainedListener) {
+        onObjectRetainedListeners.add(listener)
     }
 
-  @Synchronized fun addOnObjectRetainedListener(listener: OnObjectRetainedListener) {
-    onObjectRetainedListeners.add(listener)
-  }
-
-  @Synchronized fun removeOnObjectRetainedListener(listener: OnObjectRetainedListener) {
-    onObjectRetainedListeners.remove(listener)
-  }
-
-  /**
-   * Identical to [watch] with an empty string reference name.
-   */
-  @Deprecated(
-      "Add description parameter explaining why an object is watched to help understand leak traces.",
-      replaceWith = ReplaceWith(
-          "watch(watchedObject, \"Explain why this object should be garbage collected soon\")"
-      )
-  )
-  @Synchronized fun watch(watchedObject: Any) {
-    watch(watchedObject, "")
-  }
-
-  /**
-   * Watches the provided [watchedObject].
-   *
-   * @param description Describes why the object is watched.
-   */
-  @Synchronized fun watch(
-    watchedObject: Any,
-    description: String
-  ) {
-    if (!isEnabled()) {
-      return
-    }
-    removeWeaklyReachableObjects()
-    val key = UUID.randomUUID()
-        .toString()
-    val watchUptimeMillis = clock.uptimeMillis()
-    val reference =
-      KeyedWeakReference(watchedObject, key, description, watchUptimeMillis, queue)
-    SharkLog.d {
-      "Watching " +
-          (if (watchedObject is Class<*>) watchedObject.toString() else "instance of ${watchedObject.javaClass.name}") +
-          (if (description.isNotEmpty()) " ($description)" else "") +
-          " with key $key"
+    @Synchronized
+    fun removeOnObjectRetainedListener(listener: OnObjectRetainedListener) {
+        onObjectRetainedListeners.remove(listener)
     }
 
-    watchedObjects[key] = reference
-    checkRetainedExecutor.execute {
-      moveToRetained(key)
+    /**
+     * Identical to [watch] with an empty string reference name.
+     */
+    @Deprecated(
+            "Add description parameter explaining why an object is watched to help understand leak traces.",
+            replaceWith = ReplaceWith(
+                    "watch(watchedObject, \"Explain why this object should be garbage collected soon\")"
+            )
+    )
+    @Synchronized
+    fun watch(watchedObject: Any) {
+        watch(watchedObject, "")
     }
-  }
 
-  /**
-   * Clears all [KeyedWeakReference] that were created before [heapDumpUptimeMillis] (based on
-   * [clock] [Clock.uptimeMillis])
-   */
-  @Synchronized fun clearObjectsWatchedBefore(heapDumpUptimeMillis: Long) {
-    val weakRefsToRemove =
-      watchedObjects.filter { it.value.watchUptimeMillis <= heapDumpUptimeMillis }
-    weakRefsToRemove.values.forEach { it.clear() }
-    watchedObjects.keys.removeAll(weakRefsToRemove.keys)
-  }
+    /**
+     * Watches the provided [watchedObject].
+     *
+     * @param description Describes why the object is watched.
+     */
+    @Synchronized
+    fun watch(watchedObject: Any, description: String) {
+        if (!isEnabled()) {
+            return
+        }
+        removeWeaklyReachableObjects()
+        val key = UUID.randomUUID()
+                .toString()
+        val watchUptimeMillis = clock.uptimeMillis()
+        val reference =
+                KeyedWeakReference(watchedObject, key, description, watchUptimeMillis, queue)
+        SharkLog.d {
+            "Watching " +
+                    (if (watchedObject is Class<*>) watchedObject.toString() else "instance of ${watchedObject.javaClass.name}") +
+                    (if (description.isNotEmpty()) " ($description)" else "") +
+                    " with key $key"
+        }
 
-  /**
-   * Clears all [KeyedWeakReference]
-   */
-  @Synchronized fun clearWatchedObjects() {
-    watchedObjects.values.forEach { it.clear() }
-    watchedObjects.clear()
-  }
-
-  @Synchronized private fun moveToRetained(key: String) {
-    removeWeaklyReachableObjects()
-    val retainedRef = watchedObjects[key]
-    if (retainedRef != null) {
-      retainedRef.retainedUptimeMillis = clock.uptimeMillis()
-      onObjectRetainedListeners.forEach { it.onObjectRetained() }
+        watchedObjects[key] = reference
+        checkRetainedExecutor.execute {
+            moveToRetained(key)
+        }
     }
-  }
 
-  private fun removeWeaklyReachableObjects() {
-    // WeakReferences are enqueued as soon as the object to which they point to becomes weakly
-    // reachable. This is before finalization or garbage collection has actually happened.
-    var ref: KeyedWeakReference?
-    do {
-      ref = queue.poll() as KeyedWeakReference?
-      if (ref != null) {
-        watchedObjects.remove(ref.key)
-      }
-    } while (ref != null)
-  }
+    /**
+     * Clears all [KeyedWeakReference] that were created before [heapDumpUptimeMillis] (based on
+     * [clock] [Clock.uptimeMillis])
+     */
+    @Synchronized
+    fun clearObjectsWatchedBefore(heapDumpUptimeMillis: Long) {
+        val weakRefsToRemove =
+                watchedObjects.filter { it.value.watchUptimeMillis <= heapDumpUptimeMillis }
+        weakRefsToRemove.values.forEach { it.clear() }
+        watchedObjects.keys.removeAll(weakRefsToRemove.keys)
+    }
+
+    /**
+     * Clears all [KeyedWeakReference]
+     */
+    @Synchronized
+    fun clearWatchedObjects() {
+        watchedObjects.values.forEach { it.clear() }
+        watchedObjects.clear()
+    }
+
+    @Synchronized
+    private fun moveToRetained(key: String) {
+        removeWeaklyReachableObjects()
+        val retainedRef = watchedObjects[key]
+        if (retainedRef != null) {
+            retainedRef.retainedUptimeMillis = clock.uptimeMillis()
+            onObjectRetainedListeners.forEach { it.onObjectRetained() }
+        }
+    }
+
+    private fun removeWeaklyReachableObjects() {
+        // WeakReferences are enqueued as soon as the object to which they point to becomes weakly
+        // reachable. This is before finalization or garbage collection has actually happened.
+        var ref: KeyedWeakReference?
+        do {
+            ref = queue.poll() as KeyedWeakReference?
+            if (ref != null) {
+                watchedObjects.remove(ref.key)
+            }
+        } while (ref != null)
+    }
 }
