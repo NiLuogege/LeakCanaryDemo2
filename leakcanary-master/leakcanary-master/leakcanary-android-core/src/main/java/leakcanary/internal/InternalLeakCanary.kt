b5390b520b34493a4f502e069691c3e39e1e13a0
luoChen <hank.luo@xianghuanji.com>
2020-01-21 13:26:24 +0800

    ..

--- leakcanary-master/leakcanary-master/leakcanary-android-core/src/main/java/leakcanary/internal/InternalLeakCanary.kt
+++ leakcanary-master/leakcanary-master/leakcanary-android-core/src/main/java/leakcanary/internal/InternalLeakCanary.kt
@@ -28,228 +28,228 @@ import java.lang.reflect.Proxy
 
 internal object InternalLeakCanary : (Application) -> Unit, OnObjectRetainedListener {
 
-  private const val DYNAMIC_SHORTCUT_ID = "com.squareup.leakcanary.dynamic_shortcut"
+    private const val DYNAMIC_SHORTCUT_ID = "com.squareup.leakcanary.dynamic_shortcut"
 
-  private lateinit var heapDumpTrigger: HeapDumpTrigger
+    private lateinit var heapDumpTrigger: HeapDumpTrigger
 
-  lateinit var application: Application
+    lateinit var application: Application
 
-  // BuildConfig.LIBRARY_VERSION is stripped so this static var is how we keep it around to find
-  // it later when parsing the heap dump.
-  @Suppress("unused")
-  @JvmStatic
-  private var version = BuildConfig.LIBRARY_VERSION
+    // BuildConfig.LIBRARY_VERSION is stripped so this static var is how we keep it around to find
+    // it later when parsing the heap dump.
+    @Suppress("unused")
+    @JvmStatic
+    private var version = BuildConfig.LIBRARY_VERSION
 
-  @Volatile
-  var applicationVisible = false
-    private set
+    @Volatile
+    var applicationVisible = false
+        private set
 
-  private val testClassName by lazy {
-    application.getString(R.string.leak_canary_test_class_name)
-  }
-
-  private val isRunningTests by lazy {
-    try {
-      Class.forName(testClassName)
-      true
-    } catch (e: Exception) {
-      false
-    }
-  }
-
-  val leakDirectoryProvider: LeakDirectoryProvider by lazy {
-    LeakDirectoryProvider(application, {
-      LeakCanary.config.maxStoredHeapDumps
-    }, {
-      LeakCanary.config.requestWriteExternalStoragePermission
-    })
-  }
-
-  val leakDisplayActivityIntent: Intent
-    get() = LeakActivity.createIntent(application)
-
-  val noInstallConfig: Config
-    get() = Config(
-        dumpHeap = false,
-        referenceMatchers = emptyList(),
-        objectInspectors = emptyList(),
-        onHeapAnalyzedListener = OnHeapAnalyzedListener {}
-    )
-
-  override fun invoke(application: Application) {
-    this.application = application
-
-    AppWatcher.objectWatcher.addOnObjectRetainedListener(this)
-
-    val heapDumper = AndroidHeapDumper(application, leakDirectoryProvider)
-
-    val gcTrigger = GcTrigger.Default
-
-    val configProvider = { LeakCanary.config }
-
-    val handlerThread = HandlerThread(LEAK_CANARY_THREAD_NAME)
-    handlerThread.start()
-    val backgroundHandler = Handler(handlerThread.looper)
-
-    heapDumpTrigger = HeapDumpTrigger(
-        application, backgroundHandler, AppWatcher.objectWatcher, gcTrigger, heapDumper,
-        configProvider
-    )
-    application.registerVisibilityListener { applicationVisible ->
-      this.applicationVisible = applicationVisible
-      heapDumpTrigger.onApplicationVisibilityChanged(applicationVisible)
-    }
-    addDynamicShortcut(application)
-
-    disableDumpHeapInTests()
-  }
-
-  private fun disableDumpHeapInTests() {
-    // This is called before Application.onCreate(), so if the class is loaded through a secondary
-    // dex it might not be available yet.
-    Handler().post {
-      if (isRunningTests) {
-        SharkLog.d { "$testClassName detected in classpath, app is running tests => disabling heap dumping & analysis" }
-        LeakCanary.config = LeakCanary.config.copy(dumpHeap = false)
-      }
-    }
-  }
-
-  @Suppress("ReturnCount")
-  private fun addDynamicShortcut(application: Application) {
-    if (VERSION.SDK_INT < VERSION_CODES.N_MR1) {
-      return
-    }
-    if (!application.resources.getBoolean(R.bool.leak_canary_add_dynamic_shortcut)) {
-      return
-    }
-    if (VERSION.SDK_INT >= VERSION_CODES.O && application.packageManager.isInstantApp) {
-      // Instant Apps don't have access to ShortcutManager
-      return
+    private val testClassName by lazy {
+        application.getString(R.string.leak_canary_test_class_name)
     }
 
-    val shortcutManager = application.getSystemService(ShortcutManager::class.java)!!
-    val dynamicShortcuts = shortcutManager.dynamicShortcuts
-
-    val shortcutInstalled =
-      dynamicShortcuts.any { shortcut -> shortcut.id == DYNAMIC_SHORTCUT_ID }
-
-    if (shortcutInstalled) {
-      return
+    private val isRunningTests by lazy {
+        try {
+            Class.forName(testClassName)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
-    val mainIntent = Intent(Intent.ACTION_MAIN, null)
-    mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
-    mainIntent.setPackage(application.packageName)
-    val activities = application.packageManager.queryIntentActivities(mainIntent, 0)
-        .filter {
-          it.activityInfo.name != "leakcanary.internal.activity.LeakLauncherActivity"
+    val leakDirectoryProvider: LeakDirectoryProvider by lazy {
+        LeakDirectoryProvider(application, {
+            LeakCanary.config.maxStoredHeapDumps
+        }, {
+            LeakCanary.config.requestWriteExternalStoragePermission
+        })
+    }
+
+    val leakDisplayActivityIntent: Intent
+        get() = LeakActivity.createIntent(application)
+
+    val noInstallConfig: Config
+        get() = Config(
+                dumpHeap = false,
+                referenceMatchers = emptyList(),
+                objectInspectors = emptyList(),
+                onHeapAnalyzedListener = OnHeapAnalyzedListener {}
+        )
+
+    override fun invoke(application: Application) {
+        this.application = application
+
+        AppWatcher.objectWatcher.addOnObjectRetainedListener(this)
+
+        val heapDumper = AndroidHeapDumper(application, leakDirectoryProvider)
+
+        val gcTrigger = GcTrigger.Default
+
+        val configProvider = { LeakCanary.config }
+
+        val handlerThread = HandlerThread(LEAK_CANARY_THREAD_NAME)
+        handlerThread.start()
+        val backgroundHandler = Handler(handlerThread.looper)
+
+        heapDumpTrigger = HeapDumpTrigger(
+                application, backgroundHandler, AppWatcher.objectWatcher, gcTrigger, heapDumper,
+                configProvider
+        )
+        application.registerVisibilityListener { applicationVisible ->
+            this.applicationVisible = applicationVisible
+            heapDumpTrigger.onApplicationVisibilityChanged(applicationVisible)
+        }
+        addDynamicShortcut(application)
+
+        disableDumpHeapInTests()
+    }
+
+    private fun disableDumpHeapInTests() {
+        // This is called before Application.onCreate(), so if the class is loaded through a secondary
+        // dex it might not be available yet.
+        Handler().post {
+            if (isRunningTests) {
+                SharkLog.d { "$testClassName detected in classpath, app is running tests => disabling heap dumping & analysis" }
+                LeakCanary.config = LeakCanary.config.copy(dumpHeap = false)
+            }
+        }
+    }
+
+    @Suppress("ReturnCount")
+    private fun addDynamicShortcut(application: Application) {
+        if (VERSION.SDK_INT < VERSION_CODES.N_MR1) {
+            return
+        }
+        if (!application.resources.getBoolean(R.bool.leak_canary_add_dynamic_shortcut)) {
+            return
+        }
+        if (VERSION.SDK_INT >= VERSION_CODES.O && application.packageManager.isInstantApp) {
+            // Instant Apps don't have access to ShortcutManager
+            return
         }
 
-    if (activities.isEmpty()) {
-      return
-    }
+        val shortcutManager = application.getSystemService(ShortcutManager::class.java)!!
+        val dynamicShortcuts = shortcutManager.dynamicShortcuts
 
-    val firstMainActivity = activities.first()
-        .activityInfo
+        val shortcutInstalled =
+                dynamicShortcuts.any { shortcut -> shortcut.id == DYNAMIC_SHORTCUT_ID }
 
-    // Displayed on long tap on app icon
-    val longLabel: String
-    // Label when dropping shortcut to launcher
-    val shortLabel: String
+        if (shortcutInstalled) {
+            return
+        }
 
-    val leakActivityLabel = application.getString(R.string.leak_canary_shortcut_label)
+        val mainIntent = Intent(Intent.ACTION_MAIN, null)
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+        mainIntent.setPackage(application.packageName)
+        val activities = application.packageManager.queryIntentActivities(mainIntent, 0)
+                .filter {
+                    it.activityInfo.name != "leakcanary.internal.activity.LeakLauncherActivity"
+                }
 
-    if (activities.isEmpty()) {
-      longLabel = leakActivityLabel
-      shortLabel = leakActivityLabel
-    } else {
-      val firstLauncherActivityLabel = if (firstMainActivity.labelRes != 0) {
-        application.getString(firstMainActivity.labelRes)
-      } else {
-        application.packageManager.getApplicationLabel(application.applicationInfo)
-      }
-      val fullLengthLabel = "$firstLauncherActivityLabel $leakActivityLabel"
-      // short label should be under 10 and long label under 25
-      if (fullLengthLabel.length > 10) {
-        if (fullLengthLabel.length <= 25) {
-          longLabel = fullLengthLabel
-          shortLabel = leakActivityLabel
+        if (activities.isEmpty()) {
+            return
+        }
+
+        val firstMainActivity = activities.first()
+                .activityInfo
+
+        // Displayed on long tap on app icon
+        val longLabel: String
+        // Label when dropping shortcut to launcher
+        val shortLabel: String
+
+        val leakActivityLabel = application.getString(R.string.leak_canary_shortcut_label)
+
+        if (activities.isEmpty()) {
+            longLabel = leakActivityLabel
+            shortLabel = leakActivityLabel
         } else {
-          longLabel = leakActivityLabel
-          shortLabel = leakActivityLabel
+            val firstLauncherActivityLabel = if (firstMainActivity.labelRes != 0) {
+                application.getString(firstMainActivity.labelRes)
+            } else {
+                application.packageManager.getApplicationLabel(application.applicationInfo)
+            }
+            val fullLengthLabel = "$firstLauncherActivityLabel $leakActivityLabel"
+            // short label should be under 10 and long label under 25
+            if (fullLengthLabel.length > 10) {
+                if (fullLengthLabel.length <= 25) {
+                    longLabel = fullLengthLabel
+                    shortLabel = leakActivityLabel
+                } else {
+                    longLabel = leakActivityLabel
+                    shortLabel = leakActivityLabel
+                }
+            } else {
+                longLabel = fullLengthLabel
+                shortLabel = fullLengthLabel
+            }
         }
-      } else {
-        longLabel = fullLengthLabel
-        shortLabel = fullLengthLabel
-      }
+
+        val componentName = ComponentName(firstMainActivity.packageName, firstMainActivity.name)
+
+        val shortcutCount = dynamicShortcuts.count { shortcutInfo ->
+            shortcutInfo.activity == componentName
+        } + shortcutManager.manifestShortcuts.count { shortcutInfo ->
+            shortcutInfo.activity == componentName
+        }
+
+        if (shortcutCount >= shortcutManager.maxShortcutCountPerActivity) {
+            return
+        }
+
+        val intent = leakDisplayActivityIntent
+        intent.action = "Dummy Action because Android is stupid"
+        val shortcut = Builder(application, DYNAMIC_SHORTCUT_ID)
+                .setLongLabel(longLabel)
+                .setShortLabel(shortLabel)
+                .setActivity(componentName)
+                .setIcon(Icon.createWithResource(application, R.mipmap.leak_canary_icon))
+                .setIntent(intent)
+                .build()
+
+        try {
+            shortcutManager.addDynamicShortcuts(listOf(shortcut))
+        } catch (ignored: Throwable) {
+            SharkLog.d(ignored) {
+                "Could not add dynamic shortcut. " +
+                        "shortcutCount=$shortcutCount, " +
+                        "maxShortcutCountPerActivity=${shortcutManager.maxShortcutCountPerActivity}"
+            }
+        }
     }
 
-    val componentName = ComponentName(firstMainActivity.packageName, firstMainActivity.name)
-
-    val shortcutCount = dynamicShortcuts.count { shortcutInfo ->
-      shortcutInfo.activity == componentName
-    } + shortcutManager.manifestShortcuts.count { shortcutInfo ->
-      shortcutInfo.activity == componentName
+    override fun onObjectRetained() {
+        if (this::heapDumpTrigger.isInitialized) {
+            heapDumpTrigger.onObjectRetained()
+        }
     }
 
-    if (shortcutCount >= shortcutManager.maxShortcutCountPerActivity) {
-      return
+    fun onDumpHeapReceived(forceDump: Boolean) {
+        if (this::heapDumpTrigger.isInitialized) {
+            heapDumpTrigger.onDumpHeapReceived(forceDump)
+        }
     }
 
-    val intent = leakDisplayActivityIntent
-    intent.action = "Dummy Action because Android is stupid"
-    val shortcut = Builder(application, DYNAMIC_SHORTCUT_ID)
-        .setLongLabel(longLabel)
-        .setShortLabel(shortLabel)
-        .setActivity(componentName)
-        .setIcon(Icon.createWithResource(application, R.mipmap.leak_canary_icon))
-        .setIntent(intent)
-        .build()
-
-    try {
-      shortcutManager.addDynamicShortcuts(listOf(shortcut))
-    } catch (ignored: Throwable) {
-      SharkLog.d(ignored) {
-        "Could not add dynamic shortcut. " +
-            "shortcutCount=$shortcutCount, " +
-            "maxShortcutCountPerActivity=${shortcutManager.maxShortcutCountPerActivity}"
-      }
+    fun setEnabledBlocking(
+            componentClassName: String,
+            enabled: Boolean
+    ) {
+        val component = ComponentName(application, componentClassName)
+        val newState =
+                if (enabled) COMPONENT_ENABLED_STATE_ENABLED else COMPONENT_ENABLED_STATE_DISABLED
+        // Blocks on IPC.
+        application.packageManager.setComponentEnabledSetting(component, newState, DONT_KILL_APP)
     }
-  }
 
-  override fun onObjectRetained() {
-    if (this::heapDumpTrigger.isInitialized) {
-      heapDumpTrigger.onObjectRetained()
+    inline fun <reified T : Any> noOpDelegate(): T {
+        val javaClass = T::class.java
+        val noOpHandler = InvocationHandler { _, _, _ ->
+            // no op
+        }
+        return Proxy.newProxyInstance(
+                javaClass.classLoader, arrayOf(javaClass), noOpHandler
+        ) as T
     }
-  }
 
-  fun onDumpHeapReceived(forceDump: Boolean) {
-    if (this::heapDumpTrigger.isInitialized) {
-      heapDumpTrigger.onDumpHeapReceived(forceDump)
-    }
-  }
-
-  fun setEnabledBlocking(
-    componentClassName: String,
-    enabled: Boolean
-  ) {
-    val component = ComponentName(application, componentClassName)
-    val newState =
-      if (enabled) COMPONENT_ENABLED_STATE_ENABLED else COMPONENT_ENABLED_STATE_DISABLED
-    // Blocks on IPC.
-    application.packageManager.setComponentEnabledSetting(component, newState, DONT_KILL_APP)
-  }
-
-  inline fun <reified T : Any> noOpDelegate(): T {
-    val javaClass = T::class.java
-    val noOpHandler = InvocationHandler { _, _, _ ->
-      // no op
-    }
-    return Proxy.newProxyInstance(
-        javaClass.classLoader, arrayOf(javaClass), noOpHandler
-    ) as T
-  }
-
-  private const val LEAK_CANARY_THREAD_NAME = "LeakCanary-Heap-Dump"
+    private const val LEAK_CANARY_THREAD_NAME = "LeakCanary-Heap-Dump"
 }
